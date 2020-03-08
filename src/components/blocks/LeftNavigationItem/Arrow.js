3233b86a0e71db453781cba3daab44c515acc613
jakubpokorski <jakub.pokorski@protonmail.com>
2020-03-08 01:06:46 +0100

    Make Arrow more centered with static values

--- src/components/blocks/LeftNavigationItem/Arrow.js
+++ src/components/blocks/LeftNavigationItem/Arrow.js
@@ -3,13 +3,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Arrow = styled(FontAwesomeIcon)`
   position: absolute;
-  top: 10px;
-  right: 0;
+  top: 12px;
+  right: 10px;
   color: #7B7D8E;
   cursor: pointer;
   transform: rotate(-90deg);
   transition: 250ms transform;
-  width: 25px!important;
   display: flex;
   justify-content: center;
 `
